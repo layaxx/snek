ffe53d10729663ac839f686540dfd2c7735fb287
Yannick Lang <layaxx@gmx.de>
2024-06-12 18:07:19 +0200

    add touch event handlers

---
+++ src/event-handlers.ts
@@ -0,0 +1,129 @@
+import {
+  restartIfNeeded,
+  pauseGame,
+  startGame,
+  togglePauseStatus,
+} from "./gamelogic"
+import {
+  NAME_INPUT_SELECTOR,
+  SPEED_SLIDER_SELECTOR,
+  SPEED_DISPLAY_SELECTOR,
+  PAUSE_BUTTON_SELECTOR,
+} from "./ids"
+import { updateName } from "./scoreboard"
+import {
+  defaultSpeed,
+  setNewSpeed,
+  isRunning,
+  setNewDirection,
+  isGameOver,
+  setupGameArea,
+  setupSnek,
+} from "./state"
+import { type Direction } from "./types"
+
+function getDirection(key: string) {
+  switch (key) {
+    case "ArrowUp": // eslint-disable-line unicorn/switch-case-braces
+      return "up"
+    case "ArrowDown": // eslint-disable-line unicorn/switch-case-braces
+      return "down"
+    case "ArrowLeft": // eslint-disable-line unicorn/switch-case-braces
+      return "left"
+    default: // eslint-disable-line unicorn/switch-case-braces
+      return "right"
+  }
+}
+
+let touchstart = { x: 0, y: 0 }
+let touchend = { x: 0, y: 0 }
+
+function checkDirection(): Direction {
+  const deltaX = touchend.x - touchstart.x
+  const deltaY = touchend.y - touchstart.y
+
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    return deltaX > 0 ? "right" : "left"
+  }
+
+  return deltaY > 0 ? "down" : "up"
+}
+
+export function setupEventHandlers() {
+  document
+    .querySelector<HTMLInputElement>(NAME_INPUT_SELECTOR)
+    ?.addEventListener("change", (event) => {
+      const inputElement = event.currentTarget as HTMLInputElement
+      updateName(inputElement.value)
+    })
+  document
+    .querySelector<HTMLInputElement>(SPEED_SLIDER_SELECTOR)
+    ?.addEventListener("change", (event) => {
+      const inputElement = event.currentTarget as HTMLInputElement
+      const newSpeed = Number.parseInt(inputElement.value, 10) || defaultSpeed
+      setNewSpeed(newSpeed)
+      restartIfNeeded()
+
+      document.querySelector(SPEED_DISPLAY_SELECTOR)!.textContent =
+        String(newSpeed)
+    })
+  document.addEventListener(
+    "keydown",
+    (event) => {
+      if (isRunning()) {
+        switch (event.key) {
+          case "ArrowUp":
+          case "ArrowDown":
+          case "ArrowLeft":
+          case "ArrowRight": {
+            event.preventDefault()
+            setNewDirection(getDirection(event.key))
+            break
+          }
+
+          case "Enter":
+          case " ": {
+            event.preventDefault()
+            pauseGame()
+            break
+          }
+
+          default:
+        }
+      } else if ([" ", "Enter"].includes(event.key)) {
+        event.preventDefault()
+        if (isGameOver()) {
+          setupGameArea()
+          setupSnek()
+        }
+
+        startGame()
+      }
+    },
+    { capture: true }
+  )
+  document.addEventListener("touchstart", (event) => {
+    touchstart = {
+      x: event.changedTouches[0].screenX,
+      y: event.changedTouches[0].screenY,
+    }
+  })
+  document.addEventListener("touchend", (event) => {
+    touchend = {
+      x: event.changedTouches[0].screenX,
+      y: event.changedTouches[0].screenY,
+    }
+
+    if (isRunning()) setNewDirection(checkDirection())
+  })
+  document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+      pauseGame()
+    }
+  })
+  document
+    .querySelector<HTMLButtonElement>(PAUSE_BUTTON_SELECTOR)
+    ?.addEventListener("click", () => {
+      togglePauseStatus()
+    })
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
-import {
-  togglePauseStatus,
-  pauseGame,
-  startGame,
-  restartIfNeeded,
-} from "./gamelogic"
+import { setupEventHandlers } from "./event-handlers"
 import {
   GAME_AREA_ID,
   HIGHSCORE_ID,
@@ -11,21 +6,14 @@ import {
   NAME_INPUT_ID,
   NAME_INPUT_SELECTOR,
   PAUSE_BUTTON_ID,
-  PAUSE_BUTTON_SELECTOR,
   SCOREBOARD_ID,
   SCORE_ID,
   SPEED_DISPLAY_ID,
-  SPEED_DISPLAY_SELECTOR,
-  SPEED_SLIDER_SELECTOR,
 } from "./ids"
-import { loadScoreboard, updateName } from "./scoreboard"
+import { loadScoreboard } from "./scoreboard"
 import {
   defaultSpeed,
   gameAreaSize,
-  isGameOver,
-  isRunning,
-  setNewDirection,
-  setNewSpeed,
   setupGameArea,
   setupHighscore,
   setupSnek,
@@ -36,7 +24,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>snek</h1>
     <h2>Score: <span id="${SCORE_ID}">0</span>, Highscore: <span id="${HIGHSCORE_ID}">0</span></h2>
-    <button id="${PAUSE_BUTTON_ID}">Pause</button>
+    <button id="${PAUSE_BUTTON_ID}">Start</button>
     <div class="area" id="${GAME_AREA_ID}" style="grid-template-columns: repeat(${gameAreaSize}, 1fr)">
     </div>
 
@@ -73,76 +61,3 @@ function init() {
 }
 
 init()
-
-function getDirection(key: string) {
-  switch (key) {
-    case "ArrowUp": // eslint-disable-line unicorn/switch-case-braces
-      return "up"
-    case "ArrowDown": // eslint-disable-line unicorn/switch-case-braces
-      return "down"
-    case "ArrowLeft": // eslint-disable-line unicorn/switch-case-braces
-      return "left"
-    default: // eslint-disable-line unicorn/switch-case-braces
-      return "right"
-  }
-}
-
-function setupEventHandlers() {
-  document
-    .querySelector<HTMLInputElement>(NAME_INPUT_SELECTOR)
-    ?.addEventListener("change", (event) => {
-      const inputElement = event.currentTarget as HTMLInputElement
-      updateName(inputElement.value)
-    })
-  document
-    .querySelector<HTMLInputElement>(SPEED_SLIDER_SELECTOR)
-    ?.addEventListener("change", (event) => {
-      const inputElement = event.currentTarget as HTMLInputElement
-      const newSpeed = Number.parseInt(inputElement.value, 10) || defaultSpeed
-      setNewSpeed(newSpeed)
-      restartIfNeeded()
-
-      document.querySelector(SPEED_DISPLAY_SELECTOR)!.textContent =
-        String(newSpeed)
-    })
-  document.addEventListener(
-    "keydown",
-    (event) => {
-      if (isRunning()) {
-        switch (event.key) {
-          case "ArrowUp":
-          case "ArrowDown":
-          case "ArrowLeft":
-          case "ArrowRight": {
-            event.preventDefault()
-            setNewDirection(getDirection(event.key))
-            break
-          }
-
-          case "Enter":
-          case " ": {
-            event.preventDefault()
-            pauseGame()
-            break
-          }
-
-          default:
-        }
-      } else if ([" ", "Enter"].includes(event.key)) {
-        event.preventDefault()
-        if (isGameOver()) {
-          setupGameArea()
-          setupSnek()
-        }
-
-        startGame()
-      }
-    },
-    { capture: true }
-  )
-  document
-    .querySelector<HTMLButtonElement>(PAUSE_BUTTON_SELECTOR)
-    ?.addEventListener("click", () => {
-      togglePauseStatus()
-    })
-}
